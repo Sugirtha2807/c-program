037465ed42dfad770b15e9dc27521f2fbe332f31
Sugirtha2807 <sugirtha282004@gmail.com>
2024-11-29 11:55:31 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	char alphabet;
+	printf("enter a character:");
+	scanf("%c",&alphabet);
+	if(alphabet=='A'||alphabet=='a')
+	{
+		printf("vowel");
+	}
+	else if(alphabet=='E'||alphabet=='e')
+	{
+		printf("vowel");
+	}
+	else if(alphabet=='I'||alphabet=='i')
+	{
+		printf("vowel");
+	}
+	else if(alphabet=='O'||alphabet=='o')
+	{
+		printf("vowel");
+	}
+	else if(alphabet=='U'||alphabet=='u')
+	{
+		printf("vowel");
+	}
+	else
+	{
+		printf("constant");
+	}
+}
+
